2b3a089734281fb5836d40fb389680e3199a6580
Archit Gulve <architgulve@gmail.com>
2025-06-28 20:41:48 +0530

    Refactor upload component to enhance user interface and functionality
    - Converted the upload component to a functional component with hooks.
    - Added UI elements for selecting and scanning reports using TouchableOpacity.
    - Integrated Lottie animations and improved styling for better user experience.
    - Updated component export to follow naming conventions.

--- cura/app/(scanupload)/upload.jsx
+++ cura/app/(scanupload)/upload.jsx
@@ -1,12 +1,71 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect, useRef, useState } from "react";
+import { View, Text, TouchableOpacity, Dimensions } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import * as ImagePicker from "expo-image-picker";
+import * as MediaLibrary from "expo-media-library";
+import * as ImageManipulator from "expo-image-manipulator";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import LottieView from "lottie-react-native";
+import axios from "axios";
+import { Ionicons } from "@expo/vector-icons";
+import { router } from "expo-router";
 
-const upload = () => {
+const Upload = () => {
   return (
-    <View>
-      <Text>upload</Text>
-    </View>
-  )
-}
+    <SafeAreaView
+      style={{
+        backgroundColor: "#DFF6FB",
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <>
+        <TouchableOpacity
+          style={{
+            backgroundColor: "#008CDB",
+            paddingVertical: 15,
+            paddingHorizontal: 30,
+            borderRadius: 30,
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <Ionicons
+            name="images-outline"
+            size={24}
+            color="white"
+            style={{ marginRight: 10 }}
+          />
+          <Text style={{ color: "white", fontSize: 18, fontWeight: "bold" }}>
+            Select Report
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => router.push("/scan")}
+          style={{
+            backgroundColor: "#008CDB",
+            paddingVertical: 15,
+            paddingHorizontal: 30,
+            borderRadius: 30,
+            flexDirection: "row",
+            alignItems: "center",
+            marginTop: 20,
+          }}
+        >
+          <Ionicons
+            name="camera-outline"
+            size={24}
+            color="white"
+            style={{ marginRight: 10 }}
+          />
+          <Text style={{ color: "white", fontSize: 18, fontWeight: "bold" }}>
+            Scan Report
+          </Text>
+        </TouchableOpacity>
+      </>
+    </SafeAreaView>
+  );
+};
 
-export default upload
+export default Upload;

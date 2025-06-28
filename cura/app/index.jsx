47a4a0f34ee56490cc91e954e6b188d5f5a8b838
Archit Gulve <architgulve@gmail.com>
2025-06-28 15:10:40 +0530

    Remove unused ai.py file and update app.json to replace icon and favicon with CuraLogo.png. Add lottie-react-native dependency to package.json and package-lock.json. Enhance index.jsx to include Lottie animation and implement a redirect after 2.5 seconds.

--- cura/app/index.jsx
+++ cura/app/index.jsx
@@ -1,12 +1,39 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Pressable, Image } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { router } from "expo-router";
+import LottieView from "lottie-react-native";
+import { useEffect } from "react";
 
-const index = () => {
+const Index = () => {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push("/index_hm");
+    }, 2500);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <View>
-      <Text>index</Text>
+      <View
+        style={{
+          alignItems: "center",
+          backgroundColor: "#DFF6FB",
+        }}
+      >
+        <LottieView
+          source={require("../assets/animations/FirstPage.json")}
+          autoPlay
+          loop
+          style={{
+            width: "100%",
+            height: "100%",
+          }}
+        ></LottieView>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default index
+export default Index;
